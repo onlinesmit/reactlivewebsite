c634a77ad172da0ee6896a67cb6af2bdfe85f57f
Smit Gupta <onlinesmitg@gmail.com>
2023-09-19 00:04:21 +0530

    .

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 
 const portfolio = () => {
   return (
-    <section id = 'portfolio'>Portfolio</section>
+    <section id = 'portfolio'>
+      <h5>My Recent work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+        <div className="portfolio__item-image">
+          <img src={IMG1} alt="" />
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <div className="portfolio__item-cta"></div>
+        <a href="https://github.com" className='btn' target='_blank'>GitHub</a>
+        <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+        <div className="portfolio__item-image">
+          <img src={IMG1} alt="" />
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <div className="portfolio__item-cta"></div>
+        <a href="https://github.com" className='btn' target='_blank'>GitHub</a>
+        <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+        <div className="portfolio__item-image">
+          <img src={IMG1} alt="" />
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <div className="portfolio__item-cta"></div>
+        <a href="https://github.com" className='btn' target='_blank'>GitHub</a>
+        <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+        <div className="portfolio__item-image">
+          <img src={IMG1} alt="" />
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <div className="portfolio__item-cta"></div>
+        <a href="https://github.com" className='btn' target='_blank'>GitHub</a>
+        <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+        <div className="portfolio__item-image">
+          <img src={IMG1} alt="" />
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <div className="portfolio__item-cta"></div>
+        <a href="https://github.com" className='btn' target='_blank'>GitHub</a>
+        <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+        <div className="portfolio__item-image">
+          <img src={IMG1} alt="" />
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <div className="portfolio__item-cta"></div>
+        <a href="https://github.com" className='btn' target='_blank'>GitHub</a>
+        <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,9 +1,128 @@
 import React from 'react'
 import './services.css'
+import {AiOutlineCheck} from 'react-icons/ai'
 
 const services = () => {
   return (
-    <section id = 'services'>Services</section>
+    <section id = 'services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>
+              UI/UX design
+            </h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+          </ul>
+        </article>
+        {/* End of UI/UX design */}
+        {/* WEb development */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>
+              Web Development
+            </h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+          </ul>
+        </article>
+
+             {/* content creater */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>
+              Content Creater
+            </h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+             <li>
+              <AiOutlineCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+             </li>
+          </ul>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
